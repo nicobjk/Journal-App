a430b9e9b3677009a968452bc56e6264d4a01742
Nicobjk <nicobjk@gmail.com>
2023-06-16 13:20:20 -0300

    fin pruebas

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,43 +1,48 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
-import { Google } from '@mui/icons-material';
 import { Alert, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
+
 import { AuthLayout } from '../layout/AuthLayout';
+
 import { useForm } from '../../hooks';
 import { startGoogleSignIn, startLoginWithEmailPassword } from '../../store/auth';
-
 
 const formData = {
     email: '',
     password: ''
 }
 
+
 export const LoginPage = () => {
 
-    const { status , errorMessage } = useSelector(state => state.auth);
+    const { status, errorMessage } = useSelector(state => state.auth);
 
     const dispatch = useDispatch();
-
     const { email, password, onInputChange } = useForm(formData);
 
-    const isAuthenticated = useMemo(() => status === 'checking', [status]);
+    const isAuthenticating = useMemo(() => status === 'checking', [status]);
 
     const onSubmit = (event) => {
         event.preventDefault();
 
-        //console.log({email , password});
+        // console.log({ email, password })
         dispatch(startLoginWithEmailPassword({ email, password }));
     }
 
     const onGoogleSignIn = () => {
+        // console.log('onGoogleSignIn');
         dispatch(startGoogleSignIn());
     }
 
 
     return (
-        <AuthLayout title='Login'>
-            <form onSubmit={onSubmit} className='animate__animated animate__fadeIn animate__faster'>
+        <AuthLayout title="Login">
+            <form
+                aria-label="submit-form"
+                onSubmit={onSubmit}
+                className='animate__animated animate__fadeIn animate__faster'>
                 <Grid container>
                     <Grid item xs={12} sx={{ mt: 2 }}>
                         <TextField
@@ -57,17 +62,20 @@ export const LoginPage = () => {
                             type="password"
                             placeholder='Contraseña'
                             fullWidth
-                            name='password'
+                            name="password"
+                            inputProps={{
+                                'data-testid': 'password'
+                            }}
                             value={password}
                             onChange={onInputChange}
                         />
                     </Grid>
 
-                    <Grid 
+
+                    <Grid
                         container
                         display={!!errorMessage ? '' : 'none'}
-                        sx={{mt:1 }}
-                        >
+                        sx={{ mt: 1 }}>
                         <Grid
                             item
                             xs={12}
@@ -76,41 +84,41 @@ export const LoginPage = () => {
                         </Grid>
                     </Grid>
 
-
-
                     <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-
                         <Grid item xs={12} sm={6}>
                             <Button
-                                disabled={isAuthenticated}
-                                type='submit'
+                                disabled={isAuthenticating}
+                                type="submit"
                                 variant='contained'
-                                fullWidth
-                            >
+                                fullWidth>
                                 Login
                             </Button>
                         </Grid>
-
                         <Grid item xs={12} sm={6}>
                             <Button
+                                disabled={isAuthenticating}
                                 variant='contained'
                                 fullWidth
-                                onClick={onGoogleSignIn}
-                            >
+                                aria-label="google-btn"
+                                onClick={onGoogleSignIn}>
                                 <Google />
                                 <Typography sx={{ ml: 1 }}>Google</Typography>
                             </Button>
                         </Grid>
-
-                        <Grid container direction='row' justifyContent='end'>
-                            <Link component={RouterLink} color='inherit' to="/auth/register">
-                                Crear una cuenta
-                            </Link>
-
-                        </Grid>
                     </Grid>
+
+
+                    <Grid container direction='row' justifyContent='end'>
+                        <Link component={RouterLink} color='inherit' to="/auth/register">
+                            Crear una cuenta
+                        </Link>
+                    </Grid>
+
                 </Grid>
+
+
             </form>
+
         </AuthLayout>
     )
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,8 +2,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
+import { getEnvironments } from "../helpers/getEnvironments";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const {
+    VITE_APIKEY,
+    VITE_AUTHDOMAIN,
+    VITE_PROJECTID,
+    VITE_STORAGEBUCKET,
+    VITE_MESSAGINGSENDERID,
+    VITE_APPID,
+} = getEnvironments();
+
+
+// console.log(process.env)
+// console.log(import.meta.env )
 
 // Your web app's Firebase configuration
 // Dev / Prod
@@ -17,15 +31,26 @@ import { getFirestore } from 'firebase/firestore/lite';
 // };
 
 //Testing
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBrZbKVB7tIxlQIyZrUQuC-AxnOh35heHo",
+//     authDomain: "react-cursos-pruebas-a01a3.firebaseapp.com",
+//     projectId: "react-cursos-pruebas-a01a3",
+//     storageBucket: "react-cursos-pruebas-a01a3.appspot.com",
+//     messagingSenderId: "981085334750",
+//     appId: "1:981085334750:web:a585f827ec9ada8a3ee64f",
+//     measurementId: "G-4HKMMWQJ0C"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBrZbKVB7tIxlQIyZrUQuC-AxnOh35heHo",
-    authDomain: "react-cursos-pruebas-a01a3.firebaseapp.com",
-    projectId: "react-cursos-pruebas-a01a3",
-    storageBucket: "react-cursos-pruebas-a01a3.appspot.com",
-    messagingSenderId: "981085334750",
-    appId: "1:981085334750:web:a585f827ec9ada8a3ee64f",
-    measurementId: "G-4HKMMWQJ0C"
+    apiKey:  VITE_APIKEY,
+    authDomain: VITE_AUTHDOMAIN,
+    projectId: VITE_PROJECTID,
+    storageBucket: VITE_STORAGEBUCKET,
+    messagingSenderId: VITE_MESSAGINGSENDERID,
+    appId: VITE_APPID,
 };
+
+console.log(firebaseConfig)
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
